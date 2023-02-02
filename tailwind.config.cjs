2bd74fc48c626a813d5465aad936548403ec5572
Guilherme Athayde <guiathayde303@gmail.com>
2023-02-01 23:11:38 -0300

    Fix tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './index.html',
-  ],
+  content: ['./src/**/*.tsx', './index.html'],
   theme: {
     extend: {
       colors: {
-        background: '#09090a',
+        background: '#09090A',
       },
+
       gridTemplateRows: {
         7: 'repeat(7, minmax(0, 1fr))',
-      },
-      fontFamily: {
-        regular: 'Inter_400Regular',
-        semibold: 'Inter_600SemiBold',
-        bold: 'Inter_700Bold',
-        extrabold: 'Inter_800ExtraBold',
       },
     },
   },
